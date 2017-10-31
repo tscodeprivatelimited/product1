b5a230a1b535ba3cde29e1281393986f55e11736
trupti <hawaldartrupti@gmail.com>
2017-10-31 01:58:57 -0700

    changes in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RESTService  } from "../services/rest.service";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router : Router,private rest :RESTService) { }
 
   ngOnInit() {
   }
-
+signout(){
+  //Navigate to Home page
+     this.router.navigate(["/welcome"]);
+}
 }
